b94dc5f3493ceb5a6047cb99127fa30756a923ec
Mike Heavers <mheavers@gmail.com>
2023-09-07 11:55:00 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const recorder = require('node-record-lpcm16');
-const speech = require('@google-cloud/speech');
+const express = require("express");
+const app = express();
+const path = require("path");
+const process = require("process");
+const recorder = require("node-record-lpcm16");
+const speech = require("@google-cloud/speech");
+
+const port = 3000;
+const router = express.Router();
+const templates = path.join(process.cwd(), "templates");
+
 const client = new speech.SpeechClient();
-
-
-const encoding = 'LINEAR16'; //e.g. LINEAR16, FLAC, OGG_OPUS - https://cloud.google.com/speech-to-text/docs/reference/rest/v1/RecognitionConfig#AudioEncoding
+const encoding = "LINEAR16"; //e.g. LINEAR16, FLAC, OGG_OPUS - https://cloud.google.com/speech-to-text/docs/reference/rest/v1/RecognitionConfig#AudioEncoding
 const sampleRateHertz = 16000; //HZ - you generally need to sample more than twice the highest frequency of any sound wave you wish to capture digitally
-const languageCode = 'en-US'; //BCP-47 language code - https://cloud.google.com/speech-to-text/docs/languages
-
+const languageCode = "en-US"; //BCP-47 language code - https://cloud.google.com/speech-to-text/docs/languages
 
 const request = {
   config: {
@@ -19,28 +25,38 @@ const request = {
 
 const recognizeStream = client
   .streamingRecognize(request)
-  .on('error', console.error)
-  .on('data', data =>
+  .on("error", console.error)
+  .on("data", (data) =>
     process.stdout.write(
       data.results[0] && data.results[0].alternatives[0]
         ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
-        : '\n\nReached transcription time limit, press Ctrl+C\n'
+        : "\n\nReached transcription time limit, press Ctrl+C\n"
     )
   );
 
-// Start recording and send the microphone input to the Speech API.
-// Ensure SoX is installed, see https://www.npmjs.com/package/node-record-lpcm16#dependencies
-recorder
-  .record({
-    sampleRateHertz: sampleRateHertz,
-    threshold: 0,
-    // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
-    verbose: false,
-    recordProgram: 'rec', // Try also "arecord" or "sox"
-    silence: '10.0',
-  })
-  .stream()
-  .on('error', console.error)
-  .pipe(recognizeStream);
+const recordVoice = () => {
+  // Start recording and send the microphone input to the Speech API.
+  // Ensure SoX is installed, see https://www.npmjs.com/package/node-record-lpcm16#dependencies
+  recorder
+    .record({
+      sampleRateHertz: sampleRateHertz,
+      threshold: 0,
+      // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
+      verbose: false,
+      recordProgram: "rec", // Try also "arecord" or "sox"
+      silence: "10.0",
+    })
+    .stream()
+    .on("error", console.error)
+    .pipe(recognizeStream);
 
-console.log('Listening, press Ctrl+C to stop.');
+  console.log("Listening, press Ctrl+C to stop.");
+};
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: templates });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
